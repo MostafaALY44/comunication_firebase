d34b071c19badaee7665ae5e3df3544b4615f9d1
Ahmed Al-naggar <alnaggar98@hotmail.com>
2020-08-30 23:34:54 +0200

    color degree changed

--- src/app/shared/components/layouts/footer.component.ts
+++ src/app/shared/components/layouts/footer.component.ts
@@ -1,43 +1,50 @@
-import { Component  } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   template: `
-    <footer class="my-5 pt-5 text-muted text-center text-small">
-    <hr >
-    <p class="mb-1">&copy; 2020-2019 FC</p>
-
-    <ul class="list-inline" >
-        <li class="list-inline-item"><a style="color:#444444;text-decoration:none" (click)="redirect()" routerLink="contact-us">Contact Us</a></li>&nbsp;&nbsp;
-        <li class="list-inline-item"><a style="color:#444444;text-decoration:none" (click)="redirecta()" routerLink="about">About</a></li>&nbsp;&nbsp;
-        <li class="list-inline-item"><a style="color:#444444;text-decoration:none" (click)="privacyRedirect()" routerLink="privacy">Privacy</a></li>&nbsp;&nbsp;
-        <li class="list-inline-item"><a style="color:#444444;text-decoration:none" (click)="termsRedirect()" routerLink="terms-and-conditions">Terms</a></li>
-    </ul>
-    
-    </footer>
+    <!-- Footer -->
+    <div style= margin-top:20px></div>
+        <footer id="sticky-footer" class="py-4 bg-dark text-white-50" 
+        style=" clear: both;
+        position: relative;
+        height: 150px;
+        margin-top: 100px;
+        ">
+          <div class="container text-center">
+            <ul class="list-unstyled list-inline text-center py-2">
+              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="redirect()" routerLink="contact-us"><b>Contact Us</b></a></li>&nbsp;&nbsp;
+              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="redirecta()" routerLink="about"><b>About</b></a></li>&nbsp;&nbsp;
+              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="privacyRedirect()" routerLink="privacy"><b>Privacy</b></a></li>&nbsp;&nbsp;
+              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="termsRedirect()" routerLink="terms-and-conditions"><b>Terms</b></a></li>
+            </ul>
+            <small style="color:white">&copy; 2020-2019 FC</small>
+          </div>
+      </footer>
+    <!-- Footer -->
   `,
   //styleUrls: ['../../../../../views/auth/sb-admin-2.min.css']
 })
-export class FooterComponent  {
+export class FooterComponent {
 
   constructor(private router: Router) { }
 
-  redirect(){
+  redirect() {
     this.router.navigate(['contact-us']);
 
   }
 
-  redirecta(){
+  redirecta() {
 
     this.router.navigate(['about']);
   }
 
-  privacyRedirect(){
+  privacyRedirect() {
     this.router.navigate(['privacy']);
   }
 
-  termsRedirect(){
+  termsRedirect() {
     this.router.navigate(['terms-and-conditions']);
   }
 
