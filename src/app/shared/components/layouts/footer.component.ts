bc77f800b0473ffc2daa9921db329e4ca11626b5
Ahmed Al-naggar <alnaggar98@hotmail.com>
2020-08-31 19:34:49 +0200

    Footer postion is fixed

--- src/app/shared/components/layouts/footer.component.ts
+++ src/app/shared/components/layouts/footer.component.ts
@@ -7,17 +7,13 @@ import { Router } from '@angular/router';
     <!-- Footer -->
     <div style= margin-top:20px></div>
         <footer id="sticky-footer" class="py-4 bg-dark text-white-50" 
-        style=" clear: both;
-        position: relative;
-        height: 150px;
-        margin-top: 100px;
-        ">
+       >
           <div class="container text-center">
             <ul class="list-unstyled list-inline text-center py-2">
-              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="redirect()" routerLink="contact-us"><b>Contact Us</b></a></li>&nbsp;&nbsp;
-              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="redirecta()" routerLink="about"><b>About</b></a></li>&nbsp;&nbsp;
-              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="privacyRedirect()" routerLink="privacy"><b>Privacy</b></a></li>&nbsp;&nbsp;
-              <li class="list-inline-item"><a style="color:#FF6347;text-decoration:none" (click)="termsRedirect()" routerLink="terms-and-conditions"><b>Terms</b></a></li>
+              <li class="list-inline-item"><a style="color:#FF6347;" (click)="redirect()" routerLink="contact-us"><b>Contact Us</b></a></li>&nbsp;&nbsp;
+              <li class="list-inline-item"><a style="color:#FF6347;" (click)="redirecta()" routerLink="about"><b>About</b></a></li>&nbsp;&nbsp;
+              <li class="list-inline-item"><a style="color:#FF6347;" (click)="privacyRedirect()" routerLink="privacy"><b>Privacy</b></a></li>&nbsp;&nbsp;
+              <li class="list-inline-item"><a style="color:#FF6347;" (click)="termsRedirect()" routerLink="terms-and-conditions"><b>Terms</b></a></li>
             </ul>
             <small style="color:white">&copy; 2020-2019 FC</small>
           </div>
